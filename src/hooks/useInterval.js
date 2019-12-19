f146289b6c33549837f0398362d38e7018cfeb5a
Konstantin <konstantin.v.modin@gmail.com>
2019-12-19 17:14:44 +0100

    updates

--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -1,23 +1,24 @@
 import { useEffect, useRef } from 'react';
 
 export function useInterval(callback, delay) {
-  const savedCallback = useRef();
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
+    const savedCallback = useRef();
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
 
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      const id = setInterval(tick, delay);
-      console.log('interval number = ', id);
-      return () => {
-        clearInterval(id);
-      };
-    }
-  }, [delay]);
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        };
+    
+        if (delay !== null) {
+            const id = setInterval(tick, delay);
+            console.log('interval number = ', id);
+            return () => {
+                clearInterval(id);
+            };
+        }
+    }, [delay]);
 }
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -33,7 +33,6 @@ export const usePlayer = () => {
                 return;
             }
         }
-
         setPlayer(clonedPlayer);
     }
 
